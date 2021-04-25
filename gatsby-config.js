37f497851d58d87d5fd16cbdaae61292940d9436
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-04-25 11:23:40 +0530

    :sparkles: navbar menu removed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `COVID-19 Resource Hub`,
+    description: `A consolidation of all resources to battle COVID-19, collected from various sources.`,
+    author: `@yashkumarverma`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
